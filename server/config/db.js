06905b00450ce897b4eb10860b692a8b18042d37
Duyen Phung <zoemyphung@gmail.com>
2022-10-25 14:14:14 -0400

    Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports= 
 {
    // "URI":"mongodb://localhost/contact_list",
-    "URI":"mongodb+srv://zoemyphung:20102000@mongodbserver.txs70v4.mongodb.net/?retryWrites=true&w=majority",
+    "URI":"mongodb+srv://zoemyphung:20102000@mongodbserver.txs70v4.mongodb.net/contact_list?retryWrites=true&w=majority"
     
 }
